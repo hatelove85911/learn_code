0e650d5b059ce84f54fb1492321fad51eacd3ae9
junshen <redwolf85911@gmail.com>
2016-11-29 06:18:41 +0100

    idiomatic redux 12

--- redux/egghead2/configureStore.js
+++ redux/egghead2/configureStore.js
@@ -3,9 +3,34 @@ import {createStore} from 'redux'
 import throttle from 'lodash/throttle'
 import todoApp from './reducers/todoApp'
 
+
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch
+
+  if(!console.group){
+    return rawDispatch
+  }
+
+  return action => {
+    console.group(action.type)
+    console.log('%c previous state', 'color: grey',  store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
+
 const configureStore = () => {
   const initialState = loadState()
   const store = createStore(todoApp, initialState)
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
+
   store.subscribe(throttle(() => {
     console.log(Date.now(),"save state");
     saveState({
