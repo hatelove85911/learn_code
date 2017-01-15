39c61c79dce8edbf0f2a3c9d60ee321418a93df4
junshen <redwolf85911@gmail.com>
2017-01-15 16:42:41 +0800

    idiomatic redux 17

--- redux/egghead2/configureStore.js
+++ redux/egghead2/configureStore.js
@@ -1,44 +1,42 @@
 import {createStore} from 'redux'
 import todoApp from './reducers/todoApp'
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-  return (action) => {
-    if(typeof action.then === 'function'){
-      return action.then(rawDispatch)
-    }
-    return rawDispatch(action)
+const promise = (store) => (next) => (action) => {
+  if(typeof action.then === 'function'){
+    return action.then(next)
   }
+  return next(action)
 }
 
-
-
-const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch
-
+const logger = (store) =>(next) => {
   if(!console.group){
-    return rawDispatch
+    return next
   }
 
-  return action => {
+  return (action) => {
     console.group(action.type)
     console.log('%c previous state', 'color: grey',  store.getState())
     console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
+    const returnValue = next(action)
     console.log('%c next state', 'color: green', store.getState())
     console.groupEnd(action.type)
     return returnValue
   }
 }
 
+const wrapDispatchWithMiddleware = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(mw => store.dispatch = mw(store)(store.dispatch))
+}
+
 const configureStore = () => {
   const store = createStore(todoApp)
+  const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger)
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store)
+  wrapDispatchWithMiddleware(store, middlewares)
 
   return store
 }
