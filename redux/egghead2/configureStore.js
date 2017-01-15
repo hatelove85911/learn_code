a960fbd53e4369ee9745cd53e8ad674d20add743
junshen <redwolf85911@gmail.com>
2017-01-15 17:02:29 +0800

    idiomatic redux 18

--- redux/egghead2/configureStore.js
+++ redux/egghead2/configureStore.js
@@ -1,44 +1,17 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import createLogger from 'redux-logger'
+import promise from 'redux-promise'
 import todoApp from './reducers/todoApp'
-
-const promise = (store) => (next) => (action) => {
-  if(typeof action.then === 'function'){
-    return action.then(next)
-  }
-  return next(action)
-}
-
-const logger = (store) =>(next) => {
-  if(!console.group){
-    return next
-  }
-
-  return (action) => {
-    console.group(action.type)
-    console.log('%c previous state', 'color: grey',  store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-const wrapDispatchWithMiddleware = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(mw => store.dispatch = mw(store)(store.dispatch))
-}
 
 const configureStore = () => {
   const store = createStore(todoApp)
   const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger)
+    middlewares.push(createLogger())
   }
 
-  wrapDispatchWithMiddleware(store, middlewares)
-
-  return store
+  return createStore(todoApp, applyMiddleware(...middlewares))
 }
 
 export default configureStore
