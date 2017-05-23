36be50d1180fdd5249e239b0a0c81e9c6cf9d8e1
junshen <redwolf85911@gmail.com>
2017-05-23 12:04:48 +0200

    file upload wip

--- nodejs/file_upload/server.js
+++ nodejs/file_upload/server.js
@@ -1,40 +1,52 @@
-var http = require('http')
-var fs = require('fs')
+var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
+var util = require('util');
 
-var counter = 0
+var counter = 0;
 
-http.createServer((req, resp) => {
-  if (req.method === 'GET') {
-    if (req.url === '/upload') {
-      fs.readFile('index.html', function (err, contents) {
-        if (err) {
-          resp.writeHead(404, {
-            'Content-Type': 'text/plain'
-          })
-          resp.end('not found!!')
-        } else {
-          resp.writeHead(200, {
-            'Content-Type': 'text/html'
-          })
-          resp.end(contents)
-        }
-      })
-    }
+http
+	.createServer((req, resp) => {
+		if (req.method === 'GET') {
+			if (req.url === '/upload') {
+				fs.readFile('index.html', function(err, contents) {
+					if (err) {
+						resp.writeHead(404, {
+							'Content-Type': 'text/plain'
+						});
+						resp.end('not found!!');
+					} else {
+						resp.writeHead(200, {
+							'Content-Type': 'text/html'
+						});
+						resp.end(contents);
+					}
+				});
+			}
 
-    if(req.url === '/uploaded') {
-      fs.readdir('./fileUploaded', (err, files) => {
-        resp.end('files uploaded')
-      })
-    }
-  }
+			if (req.url === '/uploaded') {
+				fs.readdir('./fileUploaded', (err, files) => {
+					resp.end('files uploaded');
+				});
+			}
+		}
 
-  if (req.method === 'POST') {
-    if (req.url === '/uploadfile') {
-      req.pipe(fs.createWriteStream(`./fileUploaded/tmp${++counter}`))
-    }
-  }
+		if (req.method === 'POST') {
+			if (req.url === '/uploadfile') {
+				// parse a file upload
+				var form = new formidable.IncomingForm();
 
+				form.parse(req, function(err, fields, files) {
+                      fs.rename(files.file.path, `./fileUploaded/${files.file.name}`, (err, data) => {
+                          resp.write(301, {
+                              Location: '/uploaded'
+                          });
+                          resp.end();
+                      });
+				});
+			}
+		}
+	})
+	.listen(9999);
 
-}).listen(9999)
-
-console.log('server listening on port 9999')
+console.log('server listening on port 9999');
